8c1d40a357f45e3e692ead843fd30c0f13232b93
Damian Loher <damian.loher@usys.ethz.ch>
2026-08-18 12:05:04 +0200

    Removed two unnecessary files

--- romspy/UP_data_paths.py
+++
@@ -1,3 +0,0 @@
-UP_data_dir = '/net/sea/work/datasets'
-DFS_data_dir = UP_data_dir + '/gridded/atmosphere/2d/reanalysis/drakkar_forcing_set_5.2/dfs5.2_factors/'
-Dai_river_runoff_dir = UP_data_dir + '/ungridded/surface/ocean/freshwater/river_runoff_dai/'
